24b306657090e032ac5a6928d9ab43676263b884
kajal <manekajal2014@gmail.com>
2021-09-22 18:05:07 +0530

    Create Contact Details

--- src/main/java/org/addressbook/AddressBookMain.java
+++ src/main/java/org/addressbook/AddressBookMain.java
@@ -1,8 +1,36 @@
 package org.addressbook;
 
 public class AddressBookMain {
-    public static void main(String[] args){
-        System.out.println(" Welcome to Address Book Program ");
+    public String fName;
+    public String lName;
+    public String address;
+    public String city;
+    public String state;
+    public int zipCode;
+    public long mobileNo;
+    public String email;
 
+    AddressBookMain(String firstName,String lastName,String contactAddress,String contactCity,String contactState,int zipCode,long mobileNumber,String emailId){
+       this.fName=firstName;
+       this.lName=lastName;
+       this.address=contactAddress;
+       this.city=contactCity;
+       this.state=contactState;
+       this.zipCode=zipCode;
+       this.mobileNo=mobileNumber;
+       this.email=emailId;
+
+
+    }
+    void display(){
+
+        System.out.println(fName+ "||"+lName+" ||"+address+" || "+city+" || "+state+" || "+zipCode+" || "+mobileNo+" || "+email);
+
+    }
+
+
+    public static void main(String[] args){
+       AddressBookMain main = new AddressBookMain("kajal","mane","karad","pune","Kerala",123,9923866882L,"manekajal2014@gmail.com");
+        main.display();
     }
 }
